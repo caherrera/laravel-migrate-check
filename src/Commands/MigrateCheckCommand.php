e6ca2d3bb3f5641a4e8decfdce50a48ff1a64c5f
Carlos Herrera <carlos.herrera@itlinesoftware.com>
2018-11-07 12:38:49 -0300

    changing query to return number of pending migrations

--- src/Commands/MigrateCheckCommand.php
+++ src/Commands/MigrateCheckCommand.php
@@ -1,84 +1,91 @@
 <?php
+
 namespace caherrera\MigrateCheck\Commands;
 
 use Illuminate\Database\Console\Migrations\BaseCommand;
 
-class MigrateCheckCommand extends BaseCommand
-{
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'migrate:check {--database= : The database connection to use.}
+class MigrateCheckCommand extends BaseCommand {
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature
+		= 'migrate:check {--database= : The database connection to use.}
                 {--path= : The path of migrations files to be executed.}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Shows pending migrations. Command exits with non zero code if there are migrations to run';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Shows pending migrations. Command exits with non zero code if there are migrations to run';
 
-    /**
-     * The migrator instance.
-     *
-     * @var \Illuminate\Database\Migrations\Migrator
-     */
-    protected $migrator;
+	/**
+	 * The migrator instance.
+	 *
+	 * @var \Illuminate\Database\Migrations\Migrator
+	 */
+	protected $migrator;
 
-    /**
-     * Create a command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
+	/**
+	 * Create a command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		parent::__construct();
 
-        $this->migrator = app('migrator');
-    }
+		$this->migrator = app( 'migrator' );
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle()
-    {
-        $this->migrator->setConnection($this->option('database'));
+	/**
+	 * Execute the console command.
+	 *
+	 * @return int
+	 */
+	public function handle() {
+		$this->migrator->setConnection( $this->option( 'database' ) );
 
-        $files = $this->migrator->getMigrationFiles($this->getMigrationPaths());
+		$files
+			= $this->migrator->getMigrationFiles( $this->getMigrationPaths() );
 
-        $pendingMigrations = array_diff(
-            array_keys($files),
-            $this->getRanMigrations()
-        );
+		$pendingMigrations = array_diff(
+			array_keys( $files ),
+			$this->getRanMigrations()
+		);
+		if ( $this->output->isVerbose() ) {
+			$this->output->write('Pending migrations :');
+		}
+		$this->output->write( count( $pendingMigrations ) );
+		if ( $this->output->isVerbose() ) {
+			$this->output->writeln('');
+		}
 
-        if ($pendingMigrations) {
-            $this->table(['Pending migrations'], array_map(function ($migration) {
-                return [ $migration ];
-            }, $pendingMigrations));
+		if ( $pendingMigrations ) {
+			if ( $this->output->isVerbose() ) {
+				$this->table( [ 'Pending migrations' ],
+					array_map( function ( $migration ) {
+						return [ $migration ];
+					}, $pendingMigrations ) );
+			}
 
-            return count($pendingMigrations);
-        }
+			return 1;
+		}
 
-        $this->info('No pending migrations.');
+		return 0;
+	}
 
-        return 0;
-    }
+	/**
+	 * Gets ran migrations with repository check
+	 *
+	 * @return array
+	 */
+	public function getRanMigrations() {
+		if ( ! $this->migrator->repositoryExists() ) {
+			return [];
+		}
 
-    /**
-     * Gets ran migrations with repository check
-     *
-     * @return array
-     */
-    public function getRanMigrations()
-    {
-        if (! $this->migrator->repositoryExists()) {
-            return [];
-        }
-
-        return $this->migrator->getRepository()->getRan();
-    }
+		return $this->migrator->getRepository()->getRan();
+	}
 }
